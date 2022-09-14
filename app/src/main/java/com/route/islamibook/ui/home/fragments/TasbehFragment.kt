50bfc9027231fc1492bbafa644f467ab1287a70e
HalaM3abdelgowad <https://github.com/HalaM3abdelgowad>
2022-09-15 01:40:48 +0200

    add seb7a design add action of seb7a

--- app/src/main/java/com/route/islamibook/ui/home/fragments/TasbehFragment.kt
+++ app/src/main/java/com/route/islamibook/ui/home/fragments/TasbehFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.route.islamibook.R
 
 class TasbehFragment:Fragment() {
+
+    lateinit var counter:Button
+    lateinit var zekr:Button
+    var count=0
+    var generalCounter=0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +26,25 @@ class TasbehFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        counter=view.findViewById(R.id.counter)
+        zekr=view.findViewById(R.id.zekr)
+        counter.setOnClickListener {
+            count++
+            generalCounter++
+
+            if(generalCounter==3){
+                count=0
+                zekr.setText("الحمد لله")
+            }else if (generalCounter==6){
+                count=0
+                zekr.setText("لا اله الا الله ")
+            }else if (generalCounter==9){
+                count=0
+                zekr.setText("الله اكبر ")
+
+            }
+            counter.setText(count.toString())
+
+        }
     }
 }

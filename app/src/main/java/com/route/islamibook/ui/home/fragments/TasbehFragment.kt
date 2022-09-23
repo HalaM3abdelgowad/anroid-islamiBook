6e0a8bc4e6553ddb969c1220d025e425e3764bf4
HalaM3abdelgowad <https://github.com/HalaM3abdelgowad>
2022-09-23 13:38:42 +0200

    update counter of seb7a update sura details

--- app/src/main/java/com/route/islamibook/ui/home/fragments/TasbehFragment.kt
+++ app/src/main/java/com/route/islamibook/ui/home/fragments/TasbehFragment.kt
@@ -29,22 +29,31 @@ class TasbehFragment:Fragment() {
         counter=view.findViewById(R.id.counter)
         zekr=view.findViewById(R.id.zekr)
         counter.setOnClickListener {
-            count++
-            generalCounter++
+            if (generalCounter <=10) {
+                    count++
+                    generalCounter++
 
-            if(generalCounter==3){
-                count=0
-                zekr.setText("الحمد لله")
-            }else if (generalCounter==6){
-                count=0
-                zekr.setText("لا اله الا الله ")
-            }else if (generalCounter==9){
-                count=0
-                zekr.setText("الله اكبر ")
+                    if (generalCounter == 3) {
+                        count = 0
+                        zekr.setText("الحمد لله")
+                    } else if (generalCounter == 6) {
+                        count = 0
+                        zekr.setText("لا اله الا الله ")
+                    } else if (generalCounter == 9) {
+                        count = 0
+                        zekr.setText("الله اكبر ")
 
-            }
-            counter.setText(count.toString())
+                    }
+               // count++
+                    counter.setText(count.toString())
 
+
+                }else{
+                    count=0
+                    generalCounter=0
+                    counter.setText(count.toString())
+                    zekr.setText("سبحان الله")
+                }
         }
     }
 }
